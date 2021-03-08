9f4eb72bff0adc9976e95901e022c0d1a29fdd83
Bdl-1989 <dl.super@hotmail.com>
2021-03-08 22:16:06 +0800

    Update dataset_synapse.py

--- datasets/dataset_synapse.py
+++ datasets/dataset_synapse.py
@@ -6,7 +6,62 @@ import torch
 from scipy import ndimage
 from scipy.ndimage.interpolation import zoom
 from torch.utils.data import Dataset
+import pathlib
+import pandas as pd
+from torchvision import transforms as T
+from tqdm.notebook import tqdm
+import albumentations as A
+import rasterio
+from rasterio.windows import Window
+import cv2
 
+def make_grid(shape, window=256, min_overlap=32):
+    """
+        Return Array of size (N,4), where N - number of tiles,
+        2nd axis represente slices: x1,x2,y1,y2 
+    """
+    x, y = shape
+    nx = x // (window - min_overlap) + 1
+    x1 = np.linspace(0, x, num=nx, endpoint=False, dtype=np.int64)
+    x1[-1] = x - window
+    x2 = (x1 + window).clip(0, x)
+    ny = y // (window - min_overlap) + 1
+    y1 = np.linspace(0, y, num=ny, endpoint=False, dtype=np.int64)
+    y1[-1] = y - window
+    y2 = (y1 + window).clip(0, y)
+    slices = np.zeros((nx,ny, 4), dtype=np.int64)
+    
+    for i in range(nx):
+        for j in range(ny):
+            slices[i,j] = x1[i], x2[i], y1[j], y2[j]    
+    return slices.reshape(nx*ny,4)
+
+def rle_encode(im):
+    '''
+    im: numpy array, 1 - mask, 0 - background
+    Returns run length as string formated
+    '''
+    pixels = im.flatten(order = 'F')
+    pixels = np.concatenate([[0], pixels, [0]])
+    runs = np.where(pixels[1:] != pixels[:-1])[0] + 1
+    runs[1::2] -= runs[::2]
+    return ' '.join(str(x) for x in runs)
+
+def rle_decode(mask_rle, shape=(256, 256)):
+    '''
+    mask_rle: run-length as string formated (start length)
+    shape: (height,width) of array to return 
+    Returns numpy array, 1 - mask, 0 - background
+
+    '''
+    s = mask_rle.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    img = np.zeros(shape[0]*shape[1], dtype=np.uint8)
+    for lo, hi in zip(starts, ends):
+        img[lo:hi] = 1
+    return img.reshape(shape, order='F')
 
 def random_rot_flip(image, label):
     k = np.random.randint(0, 4)
@@ -49,27 +104,80 @@ class RandomGenerator(object):
 class Synapse_dataset(Dataset):
     def __init__(self, base_dir, list_dir, split, transform=None):
         self.transform = transform  # using transform in torch!
-        self.split = split
-        self.sample_list = open(os.path.join(list_dir, self.split+'.txt')).readlines()
-        self.data_dir = base_dir
+
+        # self.split = split
+        # self.sample_list = open(os.path.join(list_dir, self.split+'.txt')).readlines()
+        # self.data_dir = base_dir
+
+        self.path = pathlib.Path(base_dir)
+        self.tiff_ids = tiff_ids   # to define the train ids
+        self.overlap = 40
+        self.window = 256
+        self.transform = transform
+        self.csv = pd.read_csv((self.path / 'train.csv').as_posix(),
+                               index_col=[0])
+        self.threshold = 100
+        self.isvalid = False
+        
+        self.x, self.y, self.id = [], [], []
+        self.build_slices()
+        self.len = len(self.x)
+        self.as_tensor = T.Compose([
+            T.ToTensor(),
+            T.Normalize([0.625, 0.448, 0.688],
+                        [0.131, 0.177, 0.101]),
+        ])
+
+
 
     def __len__(self):
         return len(self.sample_list)
 
-    def __getitem__(self, idx):
-        if self.split == "train":
-            slice_name = self.sample_list[idx].strip('\n')
-            data_path = os.path.join(self.data_dir, slice_name+'.npz')
-            data = np.load(data_path)
-            image, label = data['image'], data['label']
-        else:
-            vol_name = self.sample_list[idx].strip('\n')
-            filepath = self.data_dir + "/{}.npy.h5".format(vol_name)
-            data = h5py.File(filepath)
-            image, label = data['image'][:], data['label'][:]
-
-        sample = {'image': image, 'label': label}
+    def build_slices(self):
+        self.masks = []
+        self.files = []
+        self.slices = []
+        for i, filename in enumerate(self.csv.index.values):
+            # if not filename in self.tiff_ids:
+            #     continue
+            # train all
+            filepath = (self.path /'train'/(filename+'.tiff')).as_posix()
+            self.files.append(filepath)
+            
+            # print('Transform', filename)
+            with rasterio.open(filepath, transform = rasterio.Affine(1, 0, 0, 0, 1, 0)) as dataset:
+                self.masks.append(rle_decode(self.csv.loc[filename, 'encoding'], dataset.shape))
+                slices = make_grid(dataset.shape, window=self.window, min_overlap=self.overlap)
+                
+                for slc in slices:
+                    x1,x2,y1,y2 = slc
+                    # print(slc)
+                    image = dataset.read([1,2,3],
+                            window=Window.from_slices((x1,x2),(y1,y2)))
+                    image = np.moveaxis(image, 0, -1)
+                    
+                    image = cv2.resize(image, (256, 256))
+                    masks = cv2.resize(self.masks[-1][x1:x2,y1:y2], (256, 256))
+                    
+                    if self.isvalid:
+                        self.slices.append([i,x1,x2,y1,y2])
+                        self.x.append(image)
+                        self.y.append(masks)
+                        self.id.append(filename)
+                    else:
+                        if self.masks[-1][x1:x2,y1:y2].sum() >= self.threshold or (image>32).mean() > 0.99:
+                            self.slices.append([i,x1,x2,y1,y2])
+                            
+                            self.x.append(image)
+                            self.y.append(masks)
+                            self.id.append(filename)
+    
+    # get data operation
+    def __getitem__(self, index):
+ 
+        image, mask = self.x[index], self.y[index]
+        sample = {'image': image, 'label': mask}
         if self.transform:
             sample = self.transform(sample)
-        sample['case_name'] = self.sample_list[idx].strip('\n')
+        sample['case_name'] = self.sample_list[index].strip('\n')
         return sample
